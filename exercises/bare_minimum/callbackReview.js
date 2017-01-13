a44324310f0d3d3641a8b8c255409658c8d17cd7
tenso2006 <tenso2006@gmail.com>
2017-01-12 22:16:59 -0800

    Pass callbackReview and PromiseConstructor tests

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,26 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, func) {
+  fs.readFile(filePath, 'utf8', function (err, content) {
+    if (err) {
+      func(err);
+    } else {
+      content = content.split('\n');
+      return func(err, content[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url, callback) {
-  // TODO
+  request(url, function(err, res, body) {
+    if (err) {
+      return callback(err);
+    } else {
+      return callback(err, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,12 +10,31 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise(
+    function(resolve, reject) {
+      fs.readFile(filePath, 'utf8', function (err, content) {
+        if (err) {
+          reject(err);
+        } else {
+          content = content.split('\n');
+          resolve(content[0]);
+        }
+      }); 
+    }
+  );
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise(function (resolve, reject) {
+    request(url, function(err, res, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.statusCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
